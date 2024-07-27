6b9b629e9c69e992a7c16038db5e24617bad6871
Nikesh Borkar <99666800+NikeshBorkar@users.noreply.github.com>
2024-07-27 23:59:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const parseCode = (str) => {
   // your code here
+	let n=str.length
+	let x=""
+	let arr=[]
+	for(let i=0;i<n;i++){
+		if(str[i]==0){
+			if(x==""){
+				x=""
+			}else{
+				arr.push(x)
+				x=""
+			}
+		}else{
+			x+=str[i]
+		}
+	}
+	let obj={firstName: "John", lastName: "Doe", id: "123"}
+	obj.firstName=arr[0]
+	obj.lastName=arr[1]
+	obj.id=arr[2]
+	return obj
+	
 };
 
 // Do not change the code below
